87ceeffaa46b7252624de9373a301daaf4ec817d
unknown <melinaateruel@gmail.com>
2021-06-27 18:36:13 -0300

    add travis

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//Requiero Express
+const express = require('express')
+//Instancio Express
+const app = express()
+
+//Peticion GET a la ruta principal, responde con "Esto es una prueba para IC!"
+app.get('/', (req, res) => {
+    res.status(200).send('Esto es una prueba para IC!')
+})
+
+//Arranco el Servidor en el puerto 3000
+app.listen(3000, ()=> {
+    console.log('Corriendo...')
+})
